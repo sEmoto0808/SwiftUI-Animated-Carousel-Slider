aa9a4b265286a2bd162e5ba6941cbd7cf36767c3
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-01 18:08:20 +0900

    project作成

---
+++ Carousel-Slider/Shared/Carousel_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_SliderApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
